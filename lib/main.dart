4ee1698cc9ff9c0d8b41967478ebc89ece1f0e30
zqusgurz <rexs333@naver.com>
2023-03-24 16:50:52 +0900

    first commit(bottombar)

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone_test/screen/home_screen.dart';
+import 'package:netflix_clone_test/widget/bottom_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HyukFlix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        hintColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              HomeScreen(),
+              Container(
+                child: const Center(
+                  child: Text('search'),
+                ),
+              ),
+              Container(
+                child: const Center(
+                  child: Text('save'),
+                ),
+              ),
+              Container(
+                child: const Center(
+                  child: Text('more'),
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,26 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('real home'),
+      ),
+    );
+  }
+}
